f075ee63cb02b048e13440da13eff0c992090b79
Your-Brain <144597205+Your-Brain@users.noreply.github.com>
2024-02-08 20:20:15 +0530

    Add files via upload

--- love.js
+++ love.js
@@ -5,27 +5,40 @@ let userInput = prompt("I LOVE YOU\n DO YOU LOVE ME ?");
 while (userInput === null || userInput.trim() === "") {
     userInput = prompt("I LOVE YOU,\n DO YOU LOVE ME ?");
 }
+userInput = userInput.toUpperCase();
 
-// Responds to user input with appropriate text content
-switch (userInput) {
-    case "NO":
-    case "no":
-    case "No":
-        input.textContent = "Tato valo lagbena🫥";
-        break;
-    case "YES":
-    case "yes":
-    case "Yes":
-        input.textContent = "I love you too";
-        break;
-    case "MAYBE":
-    case "maybe":
-    case "Maybe":
-        input.textContent = "Let's see what happens";
-        break;
-
-    // Add more cases as needed
-    default:
-        input.textContent = "I don't understand:(";
-        break;
+if (
+    userInput.includes("YES") ||
+    userInput.includes("HMM") ||
+    userInput.includes("I DO") ||
+    userInput.includes("I LOVE YOU") ||
+    userInput.includes("I DO")
+) {
+    input.textContent = "I love you too";
+}
+ else if (
+    userInput.includes("NO") ||
+    userInput.includes("NOT") ||
+    userInput.includes("NA")
+) {
+    img.style.display = "none";
+    input.textContent = "ok:)";
+    input.style.color = "#ff0070";
+    input.style.fontSize = "50px";
+}
+ else if (
+    userInput.startsWith("BA") ||
+    userInput.startsWith("BAN") ||
+    userInput.startsWith("GAN") ||
+    userInput.startsWith("MAG") ||
+    userInput.startsWith("KHA")||
+    userInput.startsWith("LOL")
+) {
+    img.style.display = "none";
+    input.textContent = "AUNTY JANTI HAI:(";
+    input.style.color = "#811077";
+    input.style.fontSize = "50px";
+} 
+else {
+    input.textContent = "STILL I LOVE YOU :)";
 }
